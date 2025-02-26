66e552d82afb85e5d7c6c15d87b2c7f896bd4b70
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2025-02-26 13:18:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ const __dirname = path.dirname(__filename);
 
 const app=express();
 
-app.use(express.static(path.join(__dirname,'/client/dist')))
 
-app.get('*',(req,res) => res.sendFile(path.join(__dirname,'/client/dist/index.html')))
 
 
 app.use(cors({
@@ -33,6 +31,11 @@ app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use('/',Routes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'/client/dist')));
+}
+app.use('*',(req,res) => res.sendFile(path.join(__dirname,'/client/dist/index.html')))
 
 const PORT=process.env.PORT || 8000;
 Connection();
