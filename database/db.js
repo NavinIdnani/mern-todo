89be3d8a1d5f2df787cc10c3daed1d1b5c5bb08c
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2025-02-26 13:24:53 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,15 +1,8 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 
-dotenv.config();
-const Connection =()=>{
+const Connection =async(URL)=>{
 
-    const USERNAME= process.env.DB_USERNAME;
-    const PASSWORD= process.env.DB_PASSWORD;
-
-    const MONGODB_URI=`mongodb+srv://${USERNAME}:${PASSWORD}@mern-todo.qnc63.mongodb.net/?retryWrites=true&w=majority`;
-    
-    mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI);
     mongoose.connection.on('connected',()=>{
         console.log('Database connected successfully');
     })
