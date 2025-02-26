a6819ab6b7a80ee67257159381162a7cfe7098b4
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2025-02-26 14:15:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,9 @@ app.use('/',Routes);
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname,'/client/dist')));
 }
-app.get('*',(req,res) => res.sendFile(path.join(__dirname,'/client/dist/index.html')))
+app.use('*',(req,res) => {
+ res.sendFile(path.join(__dirname,'/client/dist/index.html'));
+});
 
 const PORT=process.env.PORT || 8000;
 
